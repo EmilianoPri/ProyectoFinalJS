cdf2c903a419565e6c5463f1a5ec9c79d350932b
EmilianoPri <123654332+EmilianoPri@users.noreply.github.com>
2023-01-27 22:07:24 -0300

    Add files via upload

---
+++ Proyecto_final/Proyecto_final/Proyecto_final/Funciones.js
@@ -0,0 +1,59 @@
+
+function mostrar_Nombre_apellido() {
+    var x = document.getElementById("Nombre_Apellido");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+  function mostrar_Dirección() {
+    var x = document.getElementById("dirección");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+  function mostrar_mail() {
+    var x = document.getElementById("mail");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  
+  function mostrar_contraseña() {
+    var x = document.getElementById("Contraseña");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function mostrar_telefono() {
+    var x = document.getElementById("Telefono");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function mostrar_presentación_personal() {
+    var x = document.getElementById("presentación_personal");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  
